8eba4f65ad942f67e095295a60575f83105fccb8
Andrew Hulsizer <andrew.hulsizer@gmail.com>
2013-05-23 20:28:17 -0500

    Initial Commit

---
+++ Todo/CLMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLMAppDelegate.h
+//  Todo
+//
+//  Created by Andrew Hulsizer on 5/23/13.
+//  Copyright (c) 2013 Andrew Hulsizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Todo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Todo
+//
+//  Created by Andrew Hulsizer on 5/23/13.
+//  Copyright (c) 2013 Andrew Hulsizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CLMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLMAppDelegate class]));
+    }
+}
